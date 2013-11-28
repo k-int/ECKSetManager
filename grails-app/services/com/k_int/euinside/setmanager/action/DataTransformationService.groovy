752955e4e0cbd4d5f82e6d8f064b7780dbff840f
ChasWoodfield <chas.woodfield@k-int.co.uk>
2013-11-28 13:37:16 +0000

    We don't want to transform any records where we do not have the original data or if the record has been deleted

--- grails-app/services/com/k_int/euinside/setmanager/action/DataTransformationService.groovy
+++ grails-app/services/com/k_int/euinside/setmanager/action/DataTransformationService.groovy
@@ -39,20 +39,22 @@ class DataTransformationService extends ServiceActionBase {
 		Tracker tracker = new Tracker(Module.DATA_TRANSFORMATION.getName(), SET_MANAGER_GROUP_DATA_TRANSFORMATION);
 		tracker.start();
 		int recordsProcessed = 0;
-		Record.findAllWhere(set : set, live : false, validationStatus : Record.VALIDATION_STATUS_OK, convertedData : null).each() {
-			
-			// Increment the record processed count
-			recordsProcessed++;
-			byte [] convertedData = Transform.transformWait(set.provider.code, it.id.toString(), Format.LIDO, Format.EDM, it.originalData); 
-			if (convertedData != null) {
-				// Transformation has been successful
-				byte [] updatedRecord = (new String(convertedData, UTF8)).replace("SET_PROVIDER", providerIdentifier).getBytes(UTF8); 
-				it.convertedData = updatedRecord;
-				tracker.incrementSuccessful();
+		Record.findAllWhere(set : set, live : false, validationStatus : Record.VALIDATION_STATUS_OK, convertedData : null, deleted : false).each() {
+
+			if (it.originalData != null) {			
+				// Increment the record processed count
+				recordsProcessed++;
+				byte [] convertedData = Transform.transformWait1(set.provider.code, it.id.toString(), Format.LIDO, Format.EDM, it.originalData); 
+				if (convertedData != null) {
+					// Transformation has been successful
+					byte [] updatedRecord = (new String(convertedData, UTF8)).replace("SET_PROVIDER", providerIdentifier).getBytes(UTF8); 
+					it.convertedData = updatedRecord;
+					tracker.incrementSuccessful();
+				}
+				
+				// Not forgetting to save the record
+				saveRecord(it, "Record", it.id);
 			}
-			
-			// Not forgetting to save the record
-			saveRecord(it, "Record", it.id);
 		}
 
 		// We have finished validating so record the stats, no point recording 0 records processed though
